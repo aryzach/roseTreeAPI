8aee1bc54a17dd7e44f64139fbd151e91a0df75e
aryzach <zach_asmith@yahoo.com>
2021-09-02 21:10:51 -0400

    code to pass get_value_at test, could use tail calls to be more space efficient

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 
 class RoseTree:
+    """
+    add composite / class level tests here
+    """
     def __init__(self, value, subtrees=[]):
         self.value = value 
         self.subtrees = subtrees
@@ -14,12 +17,24 @@ class RoseTree:
 
     def get_value_at(self, list_path_to_target=[]):
         """
-        >>> ab.get_value_at()
-        5
-        >>> ab.get_value_at([0])
+        >>> abca.get_value_at()
+        2
+        >>> abca.get_value_at([0])
+        1
+        >>> ababcaa.get_value_at([0])
+        1
+        >>> ababcaa.get_value_at([1,2])
         5
         """
-        return False
+        if list_path_to_target == []:
+            return self.get_value()
+        else:
+            # use try / except
+            # could I use tail recursion here?
+            first_subtree, *rest_of_path = list_path_to_target
+            return self.subtrees[first_subtree].get_value_at(rest_of_path)
+            
+
 
 
 
@@ -34,27 +49,24 @@ if __name__ == "__main__":
     f = RoseTree(8)
 
     # depth 1
-    ab  =  RoseTree(1, [a,b])
+    ab = RoseTree(1, [a,b])
 
     # depth 2
-    abca  =  RoseTree(1, [ab,c,a])
+    abca = RoseTree(2, [ab,c,a])
 
     # depth 3
-    ababcaa  =  RoseTree(1, [ab,abca,a])
+    ababcaa = RoseTree(7, [ab,abca,a])
 
 
     import doctest
     doctest.testmod(extraglobs={
-        'a' : a,
-        'b' : RoseTree(10),
-        'c' : RoseTree(100),
-        'd' : RoseTree(2),
-        'e' : RoseTree(4),
-        'f' : RoseTree(8),
-
-        'ab'  :  RoseTree(1, [a,b]),
-
-        'abca'  :  RoseTree(1, [ab,c,a]),
-
-        'ababcaa'  :  RoseTree(1, [ab,abca,a])
+        'a'       : a,
+        'b'       : b,
+        'c'       : c,
+        'd'       : d,
+        'e'       : e,
+        'f'       : f,
+        'ab'      : ab, 
+        'abca'    : abca,
+        'ababcaa' : ababcaa 
         })
